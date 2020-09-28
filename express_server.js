3ce89e411c978eb7b3450c6b00516cc00547b3ee
Amoazzami <moazzami.azadeh@gmail.com>
2020-09-28 23:00:27 +0000

    updated version

--- express_server.js
+++ express_server.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 const PORT = 8080; // default port 8080
-app.set("view engine", "ejs");
+app.set("view engine", "ejs")
+;
 
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
@@ -36,3 +48,15 @@ app.get("/urls.json", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+function generateRandomString(length) {
+  let result = '';
+  let letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let lettersLength = letters.length;
+  for ( let i = 0; i < length; i++ ) {
+     result += letters.charAt(Math.floor(Math.random() * lettersLength));
+  }
+  return result;
+}
+
+console.log(generateRandomString(5));
